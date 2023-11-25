2cd560ab5759ad5cc80f4543a4cd78cbd97f2fe5
Rin Brk <dareka826@gmail.com>
2023-11-25 15:42:16 +0100

    Add test.c

---
+++ test.c
@@ -0,0 +1,20 @@
+#include \
+<stdio.h>
+
+typedef int i32;
+typedef unsigned int u32;
+
+x: []u32 = { 1, 2, 3 };
+z:int=0;
+
+fn main(argc: int, argv: []*char) void {
+    x: i32 = 10;
+    y: i32;
+
+    printf("Hello \"World\"!\n");
+
+    y = -0;
+    y = (max(1, 2) * 10 - 5);
+
+    return 0;
+}
