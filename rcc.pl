2de31f1de175a0656e909e9b80384ff33d7cdff7
Rin Brk <dareka826@gmail.com>
2024-01-30 19:09:06 +0100

    Add initial rcc template

---
+++ rcc.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+# C99, but:
+# - different type syntax
+# - generics
+# - short types (u8, u16, u32, i32, etc.)
+# - nullable/optional type
+# - utf8 strings
+# - local functions
+# - local structs
+
+# types syntax:
+#   C99:
+#     int *x[];
+#   RC:
+#     x: []*int;
+
+# 1. Lexer
+
+# 2. Parser
+
+# 3. Code generation
