7ce66227c0ab67fd091a31d520a0c7dbaa85d480
Rin Brk <dareka826@gmail.com>
2024-01-30 22:51:36 +0100

    Add lexer begginings

--- rcc.pl
+++ rcc.pl
@@ -15,7 +15,50 @@
 #   RC:
 #     x: []*int;
 
+sub strchr {
+    my ($str, $idx) = @_;
+    return substr($str, $idx, 1);
+}
+
 # 1. Lexer
+sub tokenize {
+    my $data = shift(@_);
+    my @tokens = ();
+
+    my $idx = 0;
+    my $data_len = length($data);
+
+    while ($idx < $data_len) {
+        my $c = strchr($data, $idx);
+        # TODO: Lexing
+
+        # Macro: from # until unescaped newline
+
+        $idx += 1;
+    }
+
+    return \@tokens;
+}
+
+tokenize(<<"EOF");
+
+#include <stdio.h>
+#include "test.h"
+
+typedef uint32_t u32;
+
+fn main(argc: int, argv: []*char) int {
+    x: int = 10;
+    y: int;
+
+    printf("abc");
+
+    x = 20;
+    return 0;
+}
+EOF
+
+exit(0);
 
 # 2. Parser
 
